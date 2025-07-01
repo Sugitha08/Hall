917e24f29821cdeafd0d05ef9f7b6679ffabd6dd
Sugitha08 <142788570+Sugitha08@users.noreply.github.com>
2025-07-01 14:43:30 +0530

    final

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Render provides the port
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 wsgi_app = app
